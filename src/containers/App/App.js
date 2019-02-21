bf3e15c670348fba3fa90c59029bacddfcd5bcd6
Whitney Burton <whitneyburton9@gmail.com>
2019-02-21 10:27:55 -0700

    Add files and packages

---
+++ src/containers/App/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { withRouter, Route, Switch } from 'react-router-dom';
+
+export class App extends Component {
+  componentDidMount = () => {
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+
+
+// export default App;
