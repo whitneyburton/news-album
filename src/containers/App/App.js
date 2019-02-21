915bedfb5187259c2c3c7621b5e717eff85c35ca
Whitney Burton <whitneyburton9@gmail.com>
2019-02-21 12:17:15 -0700

    Add fetchArticles to componentDidMount and ArticleContainer component to App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,21 +3,34 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withRouter, Route, Switch } from 'react-router-dom';
+import { ArticleContainer } from '../ArticleContainer/ArticleContainer';
+import { fetchArticles } from '../../thunks/fetchArticles';
 
 export class App extends Component {
   componentDidMount = () => {
-
+    this.props.fetchArticles();
   }
 
   render() {
     return (
       <div className="App">
         <h1>HAAAY</h1>
+        <ArticleContainer />
       </div>
     );
   }
 }
 
+export const mapStateToProps = (state) => ({
+  articles: state.articles
+})
 
+export const mapDispatchToProps = (dispatch) => ({
+  fetchArticles: () => dispatch(fetchArticles())
+})
 
-export default App;
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+
+App.propTypes = {
+  fetchArticles: PropTypes.func
+}
