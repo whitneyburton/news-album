74d691c992b78a89f3b52b681cec7690f95dd2bf
Whitney Burton <whitneyburton9@gmail.com>
2019-02-21 12:18:04 -0700

    Change fetchNotes name to fetchArticles, move function from test to main

--- src/thunks/fetchArticles.js
+++ src/thunks/fetchArticles.js
@@ -1,13 +1,14 @@
-import { setArticles, setError, toggleLoading } from '../../actions';
-import { fetchData } from '../../utils/api';
+import { setArticles, setError, toggleLoading } from '../actions';
+import { fetchData } from '../utils/api';
 
 export const fetchArticles = () => {
   return async (dispatch) => {
     try {
       dispatch(toggleLoading(true));
       const response = await fetchData('https://newsapi.org/v2/top-headlines?sources=national-geographic&apiKey=db31113f5e8d42dab3b24a559d34e086');
+      const result = await response.json();
       dispatch(toggleLoading(false));
-      dispatch(setArticles(await response.json()));
+      dispatch(setArticles(result.articles));
     } catch (error) {
       dispatch(setError(error.message));
     }
