ceaeb4a05be3c6aafcf6be4e7cba8a4af8254bbc
Whitney Burton <whitneyburton9@gmail.com>
2019-02-21 15:37:25 -0700

    Add id to SET_ARTICLES in articlesReducer

--- src/reducers/articlesReducer.js
+++ src/reducers/articlesReducer.js
@@ -1,6 +1,11 @@
+import shortid from 'shortid';
+
 export const articlesReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_ARTICLES':
+      action.articles.forEach(article => {
+        article.id = shortid.generate();
+      });
       return action.articles;
     default:
       return state;
