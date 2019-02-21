bba2c79b7bd8ee63392f8ea1bd94d1e1ea4a579c
Whitney Burton <whitneyburton9@gmail.com>
2019-02-21 13:04:26 -0700

    Delete service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.scss';
 import App from './containers/App/App';
-import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
