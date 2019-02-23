2bf702839c4471e703b66e962e5355ba56b93b24
Whitney Burton <whitneyburton9@gmail.com>
2019-02-22 18:15:32 -0700

    Create Home component, test, and scss files

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import mountains from '../../images/mountains.jpg';
+
+export const Home = () => {
+  return (
+    <div className='Home'>
+      <h1>Home</h1>
+      {mountains}
+    </div>
+  )
+}
