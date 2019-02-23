2cc4b01a0bffb209f7ec16b054f908b6219477d9
Whitney Burton <whitneyburton9@gmail.com>
2019-02-22 18:18:04 -0700

    Add Masonry to ArticleContainer and style Article and ArticleContainer

--- src/containers/ArticleContainer/ArticleContainer.js
+++ src/containers/ArticleContainer/ArticleContainer.js
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Article } from '../Article/Article';
+import Masonry from 'react-masonry-css';
 
 export class ArticleContainer extends Component {
 
   render() {
     const { articles } = this.props;
-    const articleCards = articles.map(article => {
-      return <Article article={article} />
-    })
+    const articleCards = articles.map(article => <Article article={article} />);
+    const breakpoints = {
+      default: 3,
+      1500: 2,
+      850: 1
+    };
 
     return (
-      <div className='ArticleContainer'>
+      <Masonry
+        breakpointCols={breakpoints}
+        className="ArticleContainer"
+        columnClassName="columns">
         {articleCards}
-      </div>
+      </Masonry>
     )
   }
 }
