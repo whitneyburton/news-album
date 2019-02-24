a2aec7c33e517a57dbc955317cbfbe02b997a4b0
Whitney Burton <whitneyburton9@gmail.com>
2019-02-23 18:58:18 -0700

    Add mockData for all 3 types of articles

---
+++ src/mockData/index.js
@@ -0,0 +1,62 @@
+export const mockNatGeoArticles = [
+  {
+    author: 'Erica Johnson',
+    title: '20 U.S. state parks',
+    description: 'Pretty parks!',
+    url: 'https://www.nationalgeographic.com/travel/destinations/north-america/united-states/state-parks-worth-the-trip.html',
+    urlToImage: 'https://www.nationalgeographic.com/content/dam/travel/2019-digital/state-parks-to-know/02-state-parks-waianapanapa-park-hawaii.jpg',
+    publishedAt: '2019-02-19T02:07:16.9805881Z',
+    id: 'a'
+  },
+  {
+    author: 'Douglas Main',
+    title: 'Why insect populations are plummeting',
+    description: 'Sad insects.',
+    url: 'https://www.nationalgeographic.com/animals/2019/02/why-insect-populations-are-plummeting-and-why-it-matters.html',
+    urlToImage: 'https://www.nationalgeographic.com/content/dam/animals/2019/02/insects-in-decline/01-insects-in-decline-nationalgeographic_1353541.jpg',
+    publishedAt: '2019-02-19T02:07:15.6368398Z',
+    id: 'b'
+  }
+];
+
+export const mockNewScienceArticles = [
+  {
+    author: 'Chelsea Whyte',
+    title: 'Wolves are spreading in Germany',
+    description: 'Wolves everywhere!',
+    url: 'https://www.nationalgeographic.https://www.newscientist.com/article/2194696-wolves-are-spreading-in-germany-by-moving-into-military-land//animals/2019/02/why-insect-populations-are-plummeting-and-why-it-matters.html',
+    urlToImage: 'https://images.newscientist.com/wp-content/uploads/2019/02/22145340/rm7694.jpg',
+    publishedAt: '2019-02-23T00:00:00Z',
+    id: 'c'
+  },
+  {
+    author: 'Leah Crane',
+    title: 'Four stars full of burnt nuclear ash',
+    description: 'Scary stars!',
+    url: 'https://www.newscientist.com/article/2194683-four-stars-full-of-burnt-nuclear-ash-are-speeding-through-the-galaxy/',
+    urlToImage: 'https://images.newscientist.com/wp-content/uploads/2019/02/22150921/vennes1hr.jpg',
+    publishedAt: '2019-02-22T00:00:00Z',
+    id: 'd'
+  }
+];
+
+export const mockCryptoCoinsArticles = [
+  {
+    author: 'Joseph Young',
+    title: 'Bitcoins spike',
+    description: 'So much money!',
+    url: 'https://www.ccn.com/bitcoin-price-4100-2-hours-analyst-crypto-bullish',
+    urlToImage: 'https://www.ccn.com/wp-content/uploads/2019/02/bitcoin-price-green-chart-bull-shutterstock.jpg',
+    publishedAt: '2019-02-23T14:45:23Z',
+    id: 'e'
+  },
+  {
+    author: 'Greg Thompson',
+    title: 'Ethereum Slayers',
+    description: 'They are everywhere.',
+    url: 'https://www.ccn.com/ethereum-killers-crypto-blockchain',
+    urlToImage: 'https://www.ccn.com/wp-content/uploads/2019/02/ethereum-crypto-dark-cement-shutterstock.jpg',
+    publishedAt: '2019-02-23T14:28:21Z',
+    id: 'f'
+  }
+]
