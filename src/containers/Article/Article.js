6b2b1aa205eb8ccc2a59f9ba25c232f34e7f62cf
Whitney Burton <whitneyburton9@gmail.com>
2019-02-23 17:58:27 -0700

    Add conditional for all three news sources to getPath in Article

--- src/containers/Article/Article.js
+++ src/containers/Article/Article.js
@@ -4,11 +4,22 @@ import PropTypes from 'prop-types';
 
 export class Article extends Component {
 
+  getPath = () => {
+    const { article } = this.props;
+    if (article.url.includes('nationalgeographic')) {
+      return '/national-geographic/'
+    } else if (article.url.includes('ccn')) {
+      return '/crypto-coins/'
+    } else if (article.url.includes('newscientist')) {
+      return '/new-scientist/'
+    }
+  }
+
   render() {
     const { article } = this.props;
     const title = article.title.toUpperCase();
     return (
-      <Link className='Article' to={'/national-geographic/' + article.id}>
+      <Link className='Article' to={this.getPath() + article.id}>
         <p className='Article--title'>{title}</p>
         <img className='Article--image' alt='article' src={article.urlToImage}/>
       </Link>
