3252f6915f28ab0e85ef2fa991ab9075662f538c
Whitney Burton <whitneyburton9@gmail.com>
2019-02-25 16:38:57 -0700

    Add isFavorite property to mockData

--- src/mockData/index.js
+++ src/mockData/index.js
@@ -27,7 +27,8 @@ export const mockNatGeoArticles = [
     url: 'https://www.nationalgeographic.com/travel/destinations/north-america/united-states/state-parks-worth-the-trip.html',
     urlToImage: 'https://www.nationalgeographic.com/content/dam/travel/2019-digital/state-parks-to-know/02-state-parks-waianapanapa-park-hawaii.jpg',
     publishedAt: '2019-02-19T02:07:16.9805881Z',
-    id: 'a'
+    id: 'a',
+    isFavorite: false
   },
   {
     author: 'Douglas Main',
@@ -36,7 +37,8 @@ export const mockNatGeoArticles = [
     url: 'https://www.nationalgeographic.com/animals/2019/02/why-insect-populations-are-plummeting-and-why-it-matters.html',
     urlToImage: 'https://www.nationalgeographic.com/content/dam/animals/2019/02/insects-in-decline/01-insects-in-decline-nationalgeographic_1353541.jpg',
     publishedAt: '2019-02-19T02:07:15.6368398Z',
-    id: 'b'
+    id: 'b',
+    isFavorite: false
   }
 ];
 
@@ -48,7 +50,8 @@ export const mockNewScienceArticles = [
     url: 'https://www.nationalgeographic.https://www.newscientist.com/article/2194696-wolves-are-spreading-in-germany-by-moving-into-military-land//animals/2019/02/why-insect-populations-are-plummeting-and-why-it-matters.html',
     urlToImage: 'https://images.newscientist.com/wp-content/uploads/2019/02/22145340/rm7694.jpg',
     publishedAt: '2019-02-23T00:00:00Z',
-    id: 'c'
+    id: 'c',
+    isFavorite: false
   },
   {
     author: 'Leah Crane',
@@ -57,7 +60,8 @@ export const mockNewScienceArticles = [
     url: 'https://www.newscientist.com/article/2194683-four-stars-full-of-burnt-nuclear-ash-are-speeding-through-the-galaxy/',
     urlToImage: 'https://images.newscientist.com/wp-content/uploads/2019/02/22150921/vennes1hr.jpg',
     publishedAt: '2019-02-22T00:00:00Z',
-    id: 'd'
+    id: 'd',
+    isFavorite: false
   }
 ];
 
@@ -69,7 +73,8 @@ export const mockCryptoCoinsArticles = [
     url: 'https://www.ccn.com/bitcoin-price-4100-2-hours-analyst-crypto-bullish',
     urlToImage: 'https://www.ccn.com/wp-content/uploads/2019/02/bitcoin-price-green-chart-bull-shutterstock.jpg',
     publishedAt: '2019-02-23T14:45:23Z',
-    id: 'e'
+    id: 'e',
+    isFavorite: false
   },
   {
     author: 'Greg Thompson',
@@ -78,7 +83,8 @@ export const mockCryptoCoinsArticles = [
     url: 'https://www.ccn.com/ethereum-killers-crypto-blockchain',
     urlToImage: 'https://www.ccn.com/wp-content/uploads/2019/02/ethereum-crypto-dark-cement-shutterstock.jpg',
     publishedAt: '2019-02-23T14:28:21Z',
-    id: 'f'
+    id: 'f',
+    isFavorite: false
   }
 ]
 
@@ -91,7 +97,8 @@ export const mockNatGeoArticlesObj = {
       url: 'https://www.nationalgeographic.com/travel/destinations/north-america/united-states/state-parks-worth-the-trip.html',
       urlToImage: 'https://www.nationalgeographic.com/content/dam/travel/2019-digital/state-parks-to-know/02-state-parks-waianapanapa-park-hawaii.jpg',
       publishedAt: '2019-02-19T02:07:16.9805881Z',
-      id: 'a'
+      id: 'a',
+      isFavorite: false
     },
     {
       author: 'Douglas Main',
@@ -100,7 +107,8 @@ export const mockNatGeoArticlesObj = {
       url: 'https://www.nationalgeographic.com/animals/2019/02/why-insect-populations-are-plummeting-and-why-it-matters.html',
       urlToImage: 'https://www.nationalgeographic.com/content/dam/animals/2019/02/insects-in-decline/01-insects-in-decline-nationalgeographic_1353541.jpg',
       publishedAt: '2019-02-19T02:07:15.6368398Z',
-      id: 'b'
+      id: 'b',
+      isFavorite: false
     }
   ]
 };
@@ -114,7 +122,8 @@ export const mockNewScienceArticlesObj = {
       url: 'https://www.nationalgeographic.https://www.newscientist.com/article/2194696-wolves-are-spreading-in-germany-by-moving-into-military-land//animals/2019/02/why-insect-populations-are-plummeting-and-why-it-matters.html',
       urlToImage: 'https://images.newscientist.com/wp-content/uploads/2019/02/22145340/rm7694.jpg',
       publishedAt: '2019-02-23T00:00:00Z',
-      id: 'c'
+      id: 'c',
+      isFavorite: false
     },
     {
       author: 'Leah Crane',
@@ -123,7 +132,8 @@ export const mockNewScienceArticlesObj = {
       url: 'https://www.newscientist.com/article/2194683-four-stars-full-of-burnt-nuclear-ash-are-speeding-through-the-galaxy/',
       urlToImage: 'https://images.newscientist.com/wp-content/uploads/2019/02/22150921/vennes1hr.jpg',
       publishedAt: '2019-02-22T00:00:00Z',
-      id: 'd'
+      id: 'd',
+      isFavorite: false
     }
   ]
 };
@@ -137,7 +147,8 @@ export const mockCryptoCoinsArticlesObj = {
       url: 'https://www.ccn.com/bitcoin-price-4100-2-hours-analyst-crypto-bullish',
       urlToImage: 'https://www.ccn.com/wp-content/uploads/2019/02/bitcoin-price-green-chart-bull-shutterstock.jpg',
       publishedAt: '2019-02-23T14:45:23Z',
-      id: 'e'
+      id: 'e',
+      isFavorite: false
     },
     {
       author: 'Greg Thompson',
@@ -146,7 +157,8 @@ export const mockCryptoCoinsArticlesObj = {
       url: 'https://www.ccn.com/ethereum-killers-crypto-blockchain',
       urlToImage: 'https://www.ccn.com/wp-content/uploads/2019/02/ethereum-crypto-dark-cement-shutterstock.jpg',
       publishedAt: '2019-02-23T14:28:21Z',
-      id: 'f'
+      id: 'f',
+      isFavorite: false
     }
   ]
 }
