fd0576b42adf5826fa02a4df01700b1a0fb20af2
Whitney Burton <whitneyburton9@gmail.com>
2019-02-24 23:44:27 -0700

    Add copy button functionality with CopyToClipboard component package

--- src/containers/Popup/Popup.js
+++ src/containers/Popup/Popup.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import closeicon from '../../images/close.svg';
 import staricon from '../../images/favstar.svg';
 
 export class Popup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      copied: false
+    }
+  }
+
+  toggleCopied = () => {
+    if (this.state.copied) {
+      return 'COPIED!'
+    } else {
+      return 'COPY'
+    }
+  }
 
   render() {
     const { currentArticle: article, match } = this.props;
     const previousUrl = match.path.slice(0, -4);
     const publishedDate = new Date(article.publishedAt).toString().slice(0, 15);
+    const copied = this.toggleCopied();
 
     return (
       <div className='Popup'>
@@ -27,7 +43,11 @@ export class Popup extends Component {
             <a href={article.url} target='_blank' rel='noopener noreferrer'>
               <button>READ</button>
             </a>
-            <button>COPY</button>
+            <CopyToClipboard
+              text={article.url}
+              onCopy={() => this.setState({ copied: true })}>
+              <button>{copied}</button>
+            </CopyToClipboard>
           </div>
         </div>
         <img className='Popup--image' alt='article' src={article.urlToImage} />
