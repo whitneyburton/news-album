786a0777fa56cc481d24442a549e54f683660078
Whitney Burton <whitneyburton9@gmail.com>
2019-02-24 19:04:12 -0700

    Add fetchCryptoCoins tests

---
+++ src/thunks/_tests_/fetchCryptoCoins.js
@@ -0,0 +1,43 @@
+import { fetchCryptoCoins } from '../fetchCryptoCoins';
+import { setCryptoCoins, setError, toggleLoading } from '../../actions';
+import * as api from '../../utils/api';
+import * as data from '../../mockData';
+
+describe('fetchCryptoCoins', () => {
+  const mockUrl = 'https://newsapi.org/v2/top-headlines?sources=crypto-coins-news';
+  const thunk = fetchCryptoCoins();
+  const mockDispatch = jest.fn();
+
+  beforeEach(() => {
+    api.fetchData = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve(data.mockCryptoCoinsArticles),
+      ok: true
+    }));
+  });
+
+  it('should dispatch toggleLoading with true', async () => {
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(toggleLoading(true));
+  });
+
+  it('should call fetchData with the correct params', async () => {
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(mockUrl);
+  });
+
+  it('should call setCryptoCoins with the articles', async () => {
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(setCryptoCoins(data.mockCryptoCoinsArticles));
+  });
+
+  it('should dispatch toggleLoading with true', async () => {
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(toggleLoading(false));
+  });
+
+  it('should dispatch setError with the message', async () => {
+    api.fetchData = jest.fn(() => { throw new Error('Articles not found.') });
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(setError('Articles not found.'));
+  });
+});
