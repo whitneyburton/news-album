993c73674667f7658cef9103b7929790d3e524de
Whitney Burton <whitneyburton9@gmail.com>
2019-02-26 16:27:25 -0700

    Add getFavoritesFromStorage functionality to App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { withRouter, Route, Switch } from 'react-router-dom';
 import ArticleContainer from '../ArticleContainer/ArticleContainer';
 import { Loader } from '../../components/Loader/Loader';
-import { Popup } from '../Popup/Popup';
+import Popup from '../Popup/Popup';
 import { Home } from '../../components/Home/Home';
 import { Header } from '../../components/Header/Header';
 import { Nav } from '../../components/Nav/Nav';
@@ -12,12 +12,14 @@ import { Error404 } from '../../components/Error404/Error404';
 import { fetchCryptoCoins } from '../../thunks/fetchCryptoCoins';
 import { fetchNatGeo } from '../../thunks/fetchNatGeo';
 import { fetchNewScientist } from '../../thunks/fetchNewScientist';
+import { setNatGeo, setNewScientist, setCryptoCoins } from '../../actions';
 
 export class App extends Component {
-  componentDidMount = () => {
-    this.props.fetchNatGeo();
-    this.props.fetchCryptoCoins();
-    this.props.fetchNewScientist();
+  componentDidMount = async () => {
+    await this.props.fetchNatGeo();
+    await this.props.fetchCryptoCoins();
+    await this.props.fetchNewScientist();
+    this.getFavoritesFromStorage()
   }
 
   getArticleRoute = ({ match }) => {
@@ -35,6 +37,32 @@ export class App extends Component {
       <ArticleContainer match={match} isDisabled={true} />,
       <Popup currentArticle={currentArticle} match={match} />]) :
       <Error404 />;
+  }
+
+  getFavoritesFromStorage = () => {
+    const { natGeoArticles, newScientistArticles, cryptoCoinsArticles } = this.props;
+    const parsedStorage = JSON.parse(localStorage.getItem('favorites')) || [];
+    const updatedNatGeo = natGeoArticles.map(article => {
+      if (parsedStorage.includes(article.title)) {
+        article.isFavorite = true;
+      }
+      return article;
+    })
+    this.props.setNatGeo(updatedNatGeo);
+    const updatedNewScientist = newScientistArticles.map(article => {
+      if (parsedStorage.includes(article.title)) {
+        article.isFavorite = true;
+      }
+      return article;
+    })
+    this.props.setNewScientist(updatedNewScientist);
+    const updatedCryptoCoins = cryptoCoinsArticles.map(article => {
+      if (parsedStorage.includes(article.title)) {
+        article.isFavorite = true;
+      }
+      return article;
+    })
+    this.props.setNewScientist(updatedCryptoCoins);
   }
 
   render() {
@@ -84,7 +112,10 @@ export const mapStateToProps = (state) => ({
 export const mapDispatchToProps = (dispatch) => ({
   fetchNatGeo: () => dispatch(fetchNatGeo()),
   fetchNewScientist: () => dispatch(fetchNewScientist()),
-  fetchCryptoCoins: () => dispatch(fetchCryptoCoins())
+  fetchCryptoCoins: () => dispatch(fetchCryptoCoins()),
+  setNatGeo: (articles) => dispatch(setNatGeo(articles)),
+  setNewScientist: (articles) => dispatch(setNewScientist(articles)),
+  setCryptoCoins: (articles) => dispatch(setCryptoCoins(articles))
 })
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
