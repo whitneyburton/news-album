64fdfaaca2dbee20d381fcb19fda92304596297a
Whitney Burton <whitneyburton9@gmail.com>
2019-02-26 16:58:28 -0700

    Add Link to favorites in Header, add favorites count

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,20 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
+  const favoritesNum = JSON.parse(localStorage.getItem('favorites')) || [];
+
   return (
     <Fragment>
       <h1>news<span>album</span></h1>
-      <div className='App--quote-and-favorites'>
+      <div className='Header--quote-and-favorites'>
         <div>
-          <h2 className='App--learn'>LEARN +</h2>
-          <h2 className='App--get-inspired'>GET INSPIRED.</h2>
+          <h2 className='Header--learn'>LEARN +</h2>
+          <h2 className='Header--get-inspired'>GET INSPIRED.</h2>
         </div>
-        <h3>my<span>favorites</span></h3>
+        <Link to='/favorites' className='Header--Link'>
+          <h3>my<span>favorites</span>{favoritesNum.length}</h3>
+        </Link>
       </div>
     </Fragment>
   )
