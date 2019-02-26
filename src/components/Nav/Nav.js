603223bc65d40abcfe5f613c83868628f3ea8ea8
Whitney Burton <whitneyburton9@gmail.com>
2019-02-26 16:59:05 -0700

    Add Nav styles and update naming

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,28 +3,28 @@ import { NavLink } from 'react-router-dom';
 
 export const Nav = () => {
   return (
-    <div className='App--NavLink-container'>
+    <div className='Nav--NavLink-container'>
       <NavLink
         to='/'
-        className='App--nav-btn'
+        className='Nav--nav-btn'
         activeClassName='active'>
         Home
       </NavLink>
       <NavLink
         to='/national-geographic'
-        className='App--nav-btn'
+        className='Nav--nav-btn'
         activeClassName='active'>
         National Geographic
       </NavLink>
       <NavLink
         to='/new-scientist'
-        className='App--nav-btn'
+        className='Nav--nav-btn'
         activeClassName='active'>
         The New Scientist
       </NavLink>
       <NavLink
         to='/crypto-coins'
-        className='App--nav-btn'
+        className='Nav--nav-btn'
         activeClassName='active'>
         Crypto Coins News
       </NavLink>
