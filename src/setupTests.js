a77172e502cd568ecc0fbdbbb272c01dd49fc305
Whitney Burton <whitneyburton9@gmail.com>
2019-02-26 18:20:17 -0700

    Add LocalStorage to setupTests

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,35 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() }); 
+
+class LocalStorage {
+  constructor() {
+    this.store = {}
+  }
+
+  getItem(key) {
+    return this.store[key];
+  }
+
+  setItem(key, string) {
+    this.store[key] = string;
+  }
+
+  clear() {
+    this.store = {}
+  }
+
+  removeItem(key) {
+    delete this.store[key]
+  }
+
+  hasOwnProperty(key) {
+    if(this.store[key]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+global.localStorage = new LocalStorage;
