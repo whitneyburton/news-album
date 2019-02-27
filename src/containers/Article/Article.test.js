987294b502414d056369bb9c9ea7b7b61a3b332e
Whitney Burton <whitneyburton9@gmail.com>
2019-02-26 20:41:39 -0700

    Add Article tests

--- src/containers/Article/Article.test.js
+++ src/containers/Article/Article.test.js
@@ -5,31 +5,42 @@ import * as data from '../../mockData';
 
 describe('Article', () => {
   let wrapper;
-
-  beforeEach(() => {
-    wrapper = shallow(<Article article={data.mockNatGeoArticle}/>)
-  });
+  let mockMatch;
 
   it('should match the snapshot', () => {
+    mockMatch = { path: '/favorites' }
+    wrapper = shallow(<Article article={data.mockNatGeoArticle} match={mockMatch} />)
     expect(wrapper).toMatchSnapshot();
   });
-
+  
   describe('getPath', () => {
+    it('should return a favorites path', () => {
+      mockMatch = { path: '/favorites' }
+      wrapper = shallow(<Article article={data.mockNatGeoArticle} match={mockMatch} />)
+      const expected = '/favorites/';
+      const result = wrapper.instance().getPath();
+      expect(result).toEqual(expected);
+    });
+    
     it('should return a nat geo path', () => {
+      mockMatch = { path: '/natgeo' }
+      wrapper = shallow(<Article article={data.mockNatGeoArticle} match={mockMatch} />)
       const expected = '/national-geographic/';
       const result = wrapper.instance().getPath();
       expect(result).toEqual(expected);
     });
 
     it('should return a crypto coins path', () => {
-      wrapper = shallow(<Article article={data.mockCryptoCoinsArticle} />);
+      mockMatch = { path: '/crypto' }
+      wrapper = shallow(<Article article={data.mockCryptoCoinsArticle} match={mockMatch} />);
       const expected = '/crypto-coins/';
       const result = wrapper.instance().getPath();
       expect(result).toEqual(expected);
     });
 
     it('should return a new scientist path', () => {
-      wrapper = shallow(<Article article={data.mockNewScientistArticle} />);
+      mockMatch = { path: '/newscientist' }
+      wrapper = shallow(<Article article={data.mockNewScientistArticle} match={mockMatch} />);
       const expected = '/new-scientist/';
       const result = wrapper.instance().getPath();
       expect(result).toEqual(expected);
