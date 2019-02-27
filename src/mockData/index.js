ad528d05b8c77dd9130dee54e2e832f570a8245a
Whitney Burton <whitneyburton9@gmail.com>
2019-02-26 18:20:32 -0700

    Add mockData for favorites

--- src/mockData/index.js
+++ src/mockData/index.js
@@ -1,22 +1,34 @@
 export const mockNatGeoArticle = {
-  url: 'www.nationalgeographic.com',
-  title: 'Lions and Tigers and Bears',
-  urlToImage: 'www.image.com',
-  publishedAt: '2019-02-19T02:07:16.9805881Z'
+  author: 'Douglas Main',
+  title: 'Why insect populations are plummeting',
+  description: 'Sad insects.',
+  url: 'https://www.nationalgeographic.com/animals/2019/02/why-insect-populations-are-plummeting-and-why-it-matters.html',
+  urlToImage: 'https://www.nationalgeographic.com/content/dam/animals/2019/02/insects-in-decline/01-insects-in-decline-nationalgeographic_1353541.jpg',
+  publishedAt: '2019-02-19T02:07:15.6368398Z',
+  id: 'b',
+  isFavorite: false
 }
 
 export const mockCryptoCoinsArticle = {
-  url: 'www.ccn.com',
-  title: 'Oooh coins!',
-  urlToImage: 'www.image.com',
-  publishedAt: '2019-02-19T02:07:16.9805881Z'
+  author: 'Greg Thompson',
+  title: 'Ethereum Slayers',
+  description: 'They are everywhere.',
+  url: 'https://www.ccn.com/ethereum-killers-crypto-blockchain',
+  urlToImage: 'https://www.ccn.com/wp-content/uploads/2019/02/ethereum-crypto-dark-cement-shutterstock.jpg',
+  publishedAt: '2019-02-23T14:28:21Z',
+  id: 'f',
+  isFavorite: false
 }
 
 export const mockNewScientistArticle = {
-  url: 'www.newscientist.com',
-  title: 'Science rules!',
-  urlToImage: 'www.image.com',
-  publishedAt: '2019-02-19T02:07:16.9805881Z'
+  author: 'Leah Crane',
+  title: 'Four stars full of burnt nuclear ash',
+  description: 'Scary stars!',
+  url: 'https://www.newscientist.com/article/2194683-four-stars-full-of-burnt-nuclear-ash-are-speeding-through-the-galaxy/',
+  urlToImage: 'https://images.newscientist.com/wp-content/uploads/2019/02/22150921/vennes1hr.jpg',
+  publishedAt: '2019-02-22T00:00:00Z',
+  id: 'd',
+  isFavorite: false
 }
 
 export const mockNatGeoArticles = [
@@ -39,6 +51,29 @@ export const mockNatGeoArticles = [
     publishedAt: '2019-02-19T02:07:15.6368398Z',
     id: 'b',
     isFavorite: false
+  }
+];
+
+export const mockNatGeoArticlesWithFav = [
+  {
+    author: 'Erica Johnson',
+    title: '20 U.S. state parks',
+    description: 'Pretty parks!',
+    url: 'https://www.nationalgeographic.com/travel/destinations/north-america/united-states/state-parks-worth-the-trip.html',
+    urlToImage: 'https://www.nationalgeographic.com/content/dam/travel/2019-digital/state-parks-to-know/02-state-parks-waianapanapa-park-hawaii.jpg',
+    publishedAt: '2019-02-19T02:07:16.9805881Z',
+    id: 'a',
+    isFavorite: false
+  },
+  {
+    author: 'Douglas Main',
+    title: 'Why insect populations are plummeting',
+    description: 'Sad insects.',
+    url: 'https://www.nationalgeographic.com/animals/2019/02/why-insect-populations-are-plummeting-and-why-it-matters.html',
+    urlToImage: 'https://www.nationalgeographic.com/content/dam/animals/2019/02/insects-in-decline/01-insects-in-decline-nationalgeographic_1353541.jpg',
+    publishedAt: '2019-02-19T02:07:15.6368398Z',
+    id: 'b',
+    isFavorite: true
   }
 ];
 
@@ -65,6 +100,29 @@ export const mockNewScienceArticles = [
   }
 ];
 
+export const mockNewScienceArticlesWithFav = [
+  {
+    author: 'Chelsea Whyte',
+    title: 'Wolves are spreading in Germany',
+    description: 'Wolves everywhere!',
+    url: 'https://www.nationalgeographic.https://www.newscientist.com/article/2194696-wolves-are-spreading-in-germany-by-moving-into-military-land//animals/2019/02/why-insect-populations-are-plummeting-and-why-it-matters.html',
+    urlToImage: 'https://images.newscientist.com/wp-content/uploads/2019/02/22145340/rm7694.jpg',
+    publishedAt: '2019-02-23T00:00:00Z',
+    id: 'c',
+    isFavorite: false
+  },
+  {
+    author: 'Leah Crane',
+    title: 'Four stars full of burnt nuclear ash',
+    description: 'Scary stars!',
+    url: 'https://www.newscientist.com/article/2194683-four-stars-full-of-burnt-nuclear-ash-are-speeding-through-the-galaxy/',
+    urlToImage: 'https://images.newscientist.com/wp-content/uploads/2019/02/22150921/vennes1hr.jpg',
+    publishedAt: '2019-02-22T00:00:00Z',
+    id: 'd',
+    isFavorite: true
+  }
+];
+
 export const mockCryptoCoinsArticles = [
   {
     author: 'Joseph Young',
@@ -85,6 +143,29 @@ export const mockCryptoCoinsArticles = [
     publishedAt: '2019-02-23T14:28:21Z',
     id: 'f',
     isFavorite: false
+  }
+]
+
+export const mockCryptoCoinsArticlesWithFav = [
+  {
+    author: 'Joseph Young',
+    title: 'Bitcoins spike',
+    description: 'So much money!',
+    url: 'https://www.ccn.com/bitcoin-price-4100-2-hours-analyst-crypto-bullish',
+    urlToImage: 'https://www.ccn.com/wp-content/uploads/2019/02/bitcoin-price-green-chart-bull-shutterstock.jpg',
+    publishedAt: '2019-02-23T14:45:23Z',
+    id: 'e',
+    isFavorite: false
+  },
+  {
+    author: 'Greg Thompson',
+    title: 'Ethereum Slayers',
+    description: 'They are everywhere.',
+    url: 'https://www.ccn.com/ethereum-killers-crypto-blockchain',
+    urlToImage: 'https://www.ccn.com/wp-content/uploads/2019/02/ethereum-crypto-dark-cement-shutterstock.jpg',
+    publishedAt: '2019-02-23T14:28:21Z',
+    id: 'f',
+    isFavorite: true
   }
 ]
 
