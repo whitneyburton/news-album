02fdfb590788486b7519902516dfccae13e3df79
Whitney Burton <whitneyburton9@gmail.com>
2019-02-26 19:51:58 -0700

    Style Loader and Error404

--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Error404 = () => {
   return (
     <div className='Error404'>
-      <p>Error</p>
+      <p>We're sorry,</p>
+      <p>There was an error loading this article.</p>
+      <Link to='/' className='Error404--go-back'>Go Back</Link>
     </div>
   );
 }
