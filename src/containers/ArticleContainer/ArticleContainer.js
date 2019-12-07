91f7437fd4d541b03fdc4213e25da6175313dc51
Whitney Burton <whitneyburton9@gmail.com>
2019-12-07 15:43:47 -0700

    Refactor ArticleContainer to functional comp

--- src/containers/ArticleContainer/ArticleContainer.js
+++ src/containers/ArticleContainer/ArticleContainer.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Article } from '../../components/Article/Article';
 import Masonry from 'react-masonry-css';
 
-export class ArticleContainer extends Component {
-  generateArticleCategory = () => {
-    const {
-      match,
-      natGeoArticles,
-      cryptoCoinsArticles,
-      newScientistArticles
-    } = this.props;
+export const ArticleContainer = ({
+  match,
+  isDisabled,
+  natGeoArticles,
+  cryptoCoinsArticles,
+  newScientistArticles
+}) => {
+  const generateArticleCategory = () => {
     switch (match.path) {
       case '/national-geographic':
       case '/national-geographic/:id':
@@ -35,50 +35,45 @@ export class ArticleContainer extends Component {
           ...cryptoCoinsArticles,
           ...newScientistArticles
         ];
-        return this.generateFavoriteArticles(allArticles, match);
+        return generateFavoriteArticles(allArticles, match);
       default:
         return;
     }
   };
 
-  generateFavoriteArticles = (allArticles, match) => {
+  const generateFavoriteArticles = (allArticles, match) => {
     const favoriteTitles = JSON.parse(localStorage.getItem('favorites')) || [];
     const favoriteArticles = allArticles.reduce((acc, article) => {
       favoriteTitles.forEach(title => {
-        if (article.title === title) {
-          acc.push(article);
-        }
+        if (article.title === title) acc.push(article);
       });
       return acc;
     }, []);
-    return favoriteArticles.map(article => {
-      return <Article key={article.id} article={article} match={match} />;
-    });
+    return favoriteArticles.map(article => (
+      <Article key={article.id} article={article} match={match} />
+    ));
   };
 
-  render() {
-    const articles = this.generateArticleCategory();
-    const { isDisabled } = this.props;
-    const disabledClass = isDisabled ? '--disabled' : '';
-    const breakpoints = {
-      default: 3,
-      1500: 2,
-      950: 1
-    };
+  const articles = generateArticleCategory();
+  const disabledClass = isDisabled ? '--disabled' : '';
+  const breakpoints = {
+    default: 3,
+    1500: 2,
+    950: 1
+  };
 
-    return articles.length ? (
-      <Masonry
-        breakpointCols={breakpoints}
-        className={'ArticleContainer' + disabledClass}
-        columnClassName="columns"
-      >
-        {articles}
-      </Masonry>
-    ) : (
-      <h1 className="ArticleContainer--no-faves">No favorites yet!</h1>
-    );
-  }
-}
+  return articles.length ? (
+    <Masonry
+      breakpointCols={breakpoints}
+      className={'ArticleContainer' + disabledClass}
+      columnClassName="columns"
+    >
+      {articles}
+    </Masonry>
+  ) : (
+    <h1 className="ArticleContainer--no-faves">No favorites yet!</h1>
+  );
+};
 
 export const mapStateToProps = state => ({
   natGeoArticles: state.natGeoArticles,
